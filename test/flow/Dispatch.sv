f6e76c66e9607293c2dbc81f55c08fb2ea5b6c89
Lucas Kramer <krame505@umn.edu>
2024-11-25 20:18:06 -0600

    Add test cases

--- test/flow/Dispatch.sv
+++ test/flow/Dispatch.sv
@@ -4,6 +4,7 @@ synthesized attribute errors1::Boolean;
 synthesized attribute errors2::Boolean;
 
 nonterminal UDExpr with env1, env2, errors1, errors2;
+flowtype UDExpr = forward {env1, env2}, decorate {env1, env2}, errors1 {env1}, errors2 {env1, env2};
 
 production directOverloadThing
 top::UDExpr ::= e::UDExpr
@@ -51,6 +52,7 @@ top::UDExpr ::= @e::UDExpr
 production dispatchThing3 implements DispatchOp
 top::UDExpr ::= @e::UDExpr i::Integer b::Boolean
 {
+  e.env2 = if b then [] else top.env2;
   top.errors1 = b;
   top.errors2 = i > 0;
 }
@@ -88,5 +90,42 @@ UDExpr ::= e::UDExpr
 {
   e.env1 = [];
   return shareThing(e);
+}
+}
+
+warnCode "Dispatching may require inherited attribute(s) flow:env2 on e, but these attribute(s) are supplied here after dispatching" {
+production dispatchCycle
+top::UDExpr ::= e::UDExpr
+{
+  e.env1 = top.env1;
+  local prod::DispatchOp = if null(e.env2) then dispatchThing1 else dispatchThing2;
+  forwards to prod(e);
+}
+}
+
+dispatch DispatchOp2 = UDExpr ::= e::UDExpr;
+production doimpl1 implements DispatchOp2
+top::UDExpr ::= e::UDExpr
+{
+  e.env1 = top.env1;
+  e.env2 = top.env2;
+  top.errors1 = e.errors1;
+  top.errors2 = !null(e.env1);
+}
+production doimpl2 implements DispatchOp2
+top::UDExpr ::= e::UDExpr
+{
+  e.env1 = top.env1;
+  e.env2 = top.env1;
+  top.errors1 = e.errors1;
+  top.errors2 = !null(e.env1);
+}
+
+warnCode "Dispatching may require inherited attribute(s) flow:env2 on e, but these attribute(s) are supplied here after dispatching" {
+production dispatchCycle2
+top::UDExpr ::= e::UDExpr
+{
+  local prod::DispatchOp2 = if null(e.env2) then doimpl1 else doimpl2;
+  forwards to prod(@e);
 }
 }
